0a8987737ffa590fd697467b20ff869c21ba1477
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-03-07 09:18:05 -0300

    Update num2eng.m

--- functions/num2eng.m
+++ functions/num2eng.m
@@ -1,11 +1,11 @@
-function str = num2eng(number,varargin)
+function str = num2eng(number,useSIPrefix,useMu,spaceAfterNumber,sigFigs,fullName)
 %************************************************************************************************************************************************************
 %                                                                                                                                                           *
 %   string = num2eng(number,[useSIPrefix],[useMu],[spaceAfterNumber],[sigFigs],[fullName]), where input variables in square brackets are optional - i.e.    *
 %   The function accepts anywhere from 1 to 6 inputs, inclusive. Converts an input numerical value into an engineering-formatted string, in either          *
 %   scientific format with multiples-of-three exponent, or using SI prefixes e.g. k, M, n, p etc.                                                           *
 %                                                                                                                                                           *
-%   Version: 1.0.4                                                                                                                                          *
+%   Version: 1.0.6                                                                                                                                          *
 %                                                                                                                                                           *
 %   Input variables:                                                                                                                                        *
 %       number:             The numeric value(s) to be converted to a string. If this is the only input, the string returned will be in scientific format   *
@@ -23,10 +23,10 @@ function str = num2eng(number,varargin)
 %                           num2eng(-1.2e5,1) will return the string '-120 k'                                                                               *
 %                                                                                                                                                           *
 %       useMu:              Logical. (Or can use 0/1 instead of true/false). If useSIPrefix=true, and abs(number) is >=1e-6 and <1e-9, the lower-case       *
-%                           Greek mu (unicode U+03BC) will be used. Otherwise, 'u' will be used here instead.                                               *
+%                           Greek mu (unicode U+00B5) will be used. Otherwise, 'u' will be used here instead.                                               *
 %                           Examples:                                                                                                                       *
 %                           num2eng(10e-6,true) will return the string '10 u'                                                                               *
-%                           num2eng(10e-6,true,true) will return the string '10 <mu>', where <mu> is the lower-case Greek mu character (unicode U+03BC)     *
+%                           num2eng(10e-6,true,true) will return the string '10 µ'                                                                          *
 %                                                                                                                                                           *
 %       spaceAfterNumber:   Logical. (Or can use 0/1 instead of true/false). When set to true, a space will be inserted after the numeric part of the       *
 %                           output string, after the 'e' if the output string is in scientific format, and in all cases where useSIPrefix=true, even for    *
@@ -46,7 +46,7 @@ function str = num2eng(number,varargin)
 %                           num2eng(4.5645667e-6,1,1,1,6,1) will return the string '4.56457 micro'                                                          *
 %                                                                                                                                                           *
 %   Output variables:                                                                                                                                       *
-%       str:                The output string in engineering format, or cell-matrix of strings if inut variable "number" is a matrix.                       *
+%       str:                The output string in engineering format, or cell-vector/matrix of strings if inut variable "number" is a vector/matrix.         *
 %                                                                                                                                                           *
 %   Written by Harry Dymond, University of Bristol Electrical Energy Management Group; harry.dymond@bris.ac.uk. Based on a post by Jan Simon in the thread: *
 %   https://uk.mathworks.com/matlabcentral/answers/892-engineering-notation-printed-into-files, with further inspiration from Stephen Cobeldick's num2sip   *
@@ -55,8 +55,7 @@ function str = num2eng(number,varargin)
 %                                                                                                                                                           *
 %************************************************************************************************************************************************************
     %% check inputs
-    if nargin<1 || nargin>6
-        
+    if nargin<1 || nargin>6    
         if nargin<1, errorString=sprintf('ERROR: At least one input argument is required\n');
         else         errorString=sprintf('ERROR: The maximum number of input arguments accepted is 6\n'); end
         errorString=sprintf('%s\t   Function syntax: outputString = num2eng(number,[useSIPrefix],[useMu],[spaceAfterNumber],[sigFigs],[fullName]);\n',...
@@ -65,46 +64,25 @@ function str = num2eng(number,varargin)
         error(errorString); %#ok<SPERR>
     end
     
-    optionNames    = {'useSIPrefix','useMu'  ,'spaceAfterNumber','sigFigs','fullName'};
-    optionTypes    = {'logical',    'logical','logical',         'integer','logical'};
-    optionDefaults = {false,        false,    false,             5,        false};
-    for i=1:length(optionNames)
-        options.(optionNames{i}) = optionDefaults{i};
-    end
+    optionNames = {'useSIPrefix','useMu','spaceAfterNumber','sigFigs','fullName'};
 
-    for i=1:length(varargin)
-        switch optionTypes{i}
-            case 'logical'
-                if ~islogical(varargin{i})
-                    try varargin{i}=logical(varargin{i});
-                    catch
-                        error('ERROR: %s option (%s) should be a logical value\n',num2ord(i),optionNames{i});
-                    end
-                end
-                assert(isscalar(varargin{i}),'ERROR: %s option (%s) cannot be a vector or matrix\n',num2ord(i),optionNames{i});
-            case 'integer'
-                assert(isnumeric(varargin{i})&&isscalar(varargin{i})&&isreal(varargin{i}),'ERROR: %s option (%s) should be a scalar integer\n',...
-                                                                                          num2ord(i),optionNames{i});
-                varargin{i}=round(varargin{i});
-        end
-        options.(optionNames{i}) = varargin{i};
-    end
+    if nargin > 1, i = 1; useSIPrefix       = check_logical(useSIPrefix);           else useSIPrefix        = false; end
+    if nargin > 2, i = 2; useMu             = check_logical(useMu);                 else useMu              = false; end
+    if nargin > 3, i = 3; spaceAfterNumber  = check_logical(spaceAfterNumber);      else spaceAfterNumber   = false; end
+    if nargin > 4, i = 4; sigFigs           = check_real_numeric_scalar(sigFigs);   else sigFigs            = 5;     end
+    if nargin > 5, i = 5; fullName          = check_logical(fullName);              else fullName           = false; end
     
     %% initialise SI prefix, if requested
-    if options.useSIPrefix
+    if useSIPrefix
         expValue = [  24,   21,   18,   15,   12,    9,    6,    3,  0,   -3,   -6,   -9,  -12,  -15,  -18,  -21,  -24];
-        if options.fullName
+        if fullName
             expName = {' yotta', ' zetta', ' exa', ' peta', ' tera', ' giga', ' mega', ' kilo', '', ...
                        ' milli', ' micro', ' nano', ' pico', ' femto', ' atto', ' zepto', ' yocto'};
         else
             expName = {' Y', ' Z', ' E', ' P', ' T', ' G', ' M', ' k', '', ' m', ' u', ' n', ' p', ' f', ' a', ' z', ' y'};
-            if options.useMu
-                expName{11}=[' ' char(956)]; % char(956) is the lower case greek "mu" character
-            end
-        end      
-        if options.spaceAfterNumber
-            expName{9} = ' ';
+            if useMu, expName{11} = [' ' char(181)]; end % char(181) is the lower case Greek "mu" character
         end
+        if spaceAfterNumber, expName{9} = ' '; end
     end
     
     %% Do the conversion
@@ -128,14 +106,14 @@ function str = num2eng(number,varargin)
     %% Subroutines    
     function str = num2eng_part1(number)
         if ~isreal(number)
-        % handle imaginary numbers
+        %% handle imaginary numbers
             if imag(number)==0
                 str = num2eng_part2(real(number));
             else
-                SIPrefixOption = options.useSIPrefix;
-                options.useSIPrefix = false;
-                spaceOption = options.spaceAfterNumber;
-                options.spaceAfterNumber = false;
+                SIPrefixOption = useSIPrefix;
+                useSIPrefix = false;
+                spaceOption = spaceAfterNumber;
+                spaceAfterNumber = false;
                 str = num2eng_part2(real(number));
                 if imag(number)>0, str = [str '+']; end
                 if spaceOption
@@ -143,12 +121,12 @@ function str = num2eng(number,varargin)
                 else
                     str = [str num2eng_part2(imag(number)) 'i'];
                 end
-                options.spaceAfterNumber = spaceOption;
-                options.useSIPrefix = SIPrefixOption;
+                spaceAfterNumber = spaceOption;
+                useSIPrefix = SIPrefixOption;
                 return
             end
         else  
-        % handle all other cases
+        %% handle all other cases
             str = num2eng_part2(number);
         end
     end
@@ -165,53 +143,72 @@ function str = num2eng(number,varargin)
         exponent = 3 * floor(log10(abs(number)) / 3);
         mantissa = number / (10 ^ exponent);
         
-        mantissa = round(mantissa,options.sigFigs,'significant');
+        mantissa = round(mantissa,sigFigs,'significant');
         if mantissa>=1000
             exponent = exponent+3;
             mantissa = mantissa/1000;
         end
         
-        if options.useSIPrefix, expIndex = (exponent == expValue); end
+        if useSIPrefix, expIndex = (exponent == expValue); end
         
-        if options.useSIPrefix && any(expIndex)
-            str = sprintf('%.*g%s',max(options.sigFigs,1+floor(log10(abs(mantissa)))), mantissa, expName{expIndex});
+        if useSIPrefix && any(expIndex)
+            str = sprintf('%.*g%s',max(sigFigs,1+floor(log10(abs(mantissa)))), mantissa, expName{expIndex});
         else
             if exponent~=0
-                str = sprintf('%.*ge%+d',max(options.sigFigs,1+floor(log10(abs(mantissa)))), mantissa, exponent);
+                str = sprintf('%.*ge%+d',max(sigFigs,1+floor(log10(abs(mantissa)))), mantissa, exponent);
             else
-                str = sprintf('%.*g', options.sigFigs, mantissa);
+                str = sprintf('%.*g', sigFigs, mantissa);
             end
             add_space;
         end
         %% add space to end of string if required
         function add_space()
-            if options.spaceAfterNumber, str = [str ' ']; end
+            if spaceAfterNumber, str = [str ' ']; end
         end
     end
 
 
     function str = num2ord(num)
         switch num
-            case 1
-                str = 'First';
-            case 2
-                str = 'Second';
-            case 3
-                str = 'Third';
-            case 4
-                str = 'Fourth';
-            case 5
-                str = 'Fifth';
-            case 6
-                str = 'Sixth';
-            case 7
-                str = 'Seventh';
-            case 8
-                str = 'Eighth';
-            case 9
-                str = 'Ninth';
-            case 10
-                str = 'Tenth';
+            case 1,  str = 'First';
+            case 2,  str = 'Second';
+            case 3,  str = 'Third';
+            case 4,  str = 'Fourth';
+            case 5,  str = 'Fifth';
+            case 6,  str = 'Sixth';
+            case 7,  str = 'Seventh';
+            case 8,  str = 'Eighth';
+            case 9,  str = 'Ninth';
+            case 10, str = 'Tenth';
         end
+    end
+
+
+    function var = check_logical(var)
+        if ~islogical(var)
+            try var = logical(var);
+            catch
+                try
+                    error('ERROR: %s option (%s) should be a logical value\n',num2ord(i),optionNames{i});
+                catch ME
+                    throwAsCaller(ME);
+                end
+            end
+        end
+        try
+            assert(isscalar(var),'ERROR: %s option (%s) cannot be a vector or matrix\n',num2ord(i),optionNames{i});
+        catch ME
+            throwAsCaller(ME);
+        end
+    end
+
+
+    function var = check_real_numeric_scalar(var)
+        try
+            assert(isnumeric(var)&&isscalar(var)&&isreal(var),'ERROR: %s option (%s) should be a scalar integer\n',num2ord(i),optionNames{i});
+        catch ME
+            throwAsCaller(ME);
+        end        
+        var = round(var);
     end
 end
