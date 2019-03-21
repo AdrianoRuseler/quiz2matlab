fdc66b432db244731e7606f4412e1f188cd5edf6
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-03-21 10:22:17 -0300

    Update getpsimnet.m
    Generates net file from psim file;

--- functions/getpsimnet.m
+++ functions/getpsimnet.m
@@ -2,18 +2,20 @@
 % Get net file parameters
 function [circuit] = getpsimnet(circuit)
 
-circuit.PSIMCMD.net.file = [circuit.PSIMCMD.simsdir '\' circuit.PSIMCMD.name '.cct']; % net file
-circuit.PSIMCMD.file = [circuit.PSIMCMD.simsdir '\' circuit.PSIMCMD.name '.psimsch']; % PSIM file
+circuit.PSIMCMD.net.file = [circuit.PSIMCMD.simsdir circuit.PSIMCMD.name '.cct']; % net file
+circuit.PSIMCMD.file = [circuit.PSIMCMD.simsdir circuit.PSIMCMD.name '.psimsch']; % PSIM file
 
-if ~exist(circuit.PSIMCMD.net.file,'file') % File exists?
-    disp(['File not found: ' circuit.PSIMCMD.net.file])
-    system(['PsimCmd -i ' circuit.PSIMCMD.file ' -o ' circuit.PSIMCMD.net.file ' -Net']); % Generates the netlist
+if ~exist(circuit.PSIMCMD.file,'file') % File exists?
+    disp(['File not found: ' circuit.PSIMCMD.file])    
+    return
 end
+
+system(['PsimCmd -i ' circuit.PSIMCMD.file ' -o ' circuit.PSIMCMD.net.file ' -Net']); % Generates the netlist
 
 fileID = fopen(circuit.PSIMCMD.net.file);
 if fileID == -1
    disp(['Could not open: ' circuit.PSIMCMD.net.file])    
-    return;
+    return
 end
 
 l=1;
