d0279ccb6c1d84f361321eb5c8b808f1265c592b
Adriano Ruseler <adrianoruseler@gamil.com>
2019-03-22 17:47:18 -0300

    Update real2eng.m
    Fixed for inf case

--- functions/real2eng.m
+++ functions/real2eng.m
@@ -12,16 +12,24 @@ if ~isreal(number) % Is this number real?
     return % only real numbers
 end
 
+
 exponent = 3*floor(log10(abs(number))/3); % find exponent
 mantissa = number/(10^exponent); % find mantissa
 
 expHTML = {' y',' z', ' a', ' f', ' p', ' n', ' &micro;', ' m', ' ', ' k', ' M', ' G', ' T', ' P', ' E', ' Z',' Y'};
 % expName = {' y',' z', ' a', ' f', ' p', ' n', ' u', ' m', '', ' k', ' M', ' G', ' T', ' P', ' E', ' Z',' Y'};
-expstr= [expHTML{exponent/3 +9} unitstr];
+
+if isinf(exponent) % If is inf!
+    expstr=[' ' unitstr];
+    numstr = '0';
+else
+    expstr= [expHTML{exponent/3 +9} unitstr];
+    numstr = strrep(sprintf('%3.3f',mantissa),'.',',');
+end
+
 
 % expValue = [-24,-21,-18,-15,-12,-9,-6,-3,0,3,6,9,12,15,18,21,24];
 % numstr = strrep(num2str(mantissa,'%3.3f'),'.',',');
-numstr = strrep(sprintf('%3.3f',mantissa),'.',',');
 
 str = [numstr expstr];
 
