0f97e809672dbc443eced7309c2a99b51d5a18df
Adriano Ruseler <adrianoruseler@gamil.com>
2019-04-10 13:57:25 -0300

    Update rawltspice.m

--- functions/rawltspice.m
+++ functions/rawltspice.m
@@ -1,22 +1,23 @@
 
-function data = rawltspice(filename)
+function data = rawltspice(data)
 % Elapsed time is 0.053146 seconds.
+
 
 tstart = tic;
 fprintf('Extracting LTspice ASCII results ...\n');
     
-if ~exist(filename,'file') % Verifica se existe o arquivo
-    disp(['File not found:' filename ])
-    data=[];
+if ~exist(data.filename,'file') % Verifica se existe o arquivo
+    disp(['File not found:' data.filename ])
+%     data=[];
     return;
 end
 
 % tic
 
-fileID = fopen(filename,'rb'); % Opens file
+fileID = fopen(data.filename,'rb'); % Opens file
 if fileID == -1
-    disp(['Could not open:' filename ])
-    data=[];
+    disp(['Could not open:' data.filename ])
+%     data=[];
     return;
 end
 
@@ -24,12 +25,12 @@ Line=1;
 tline = cell(1,20); % Initial header size guess
 tmpchar = fgetl(fileID);
 if ~contains(tmpchar,'Title:') % Title
-    disp('binary data!') % frewind(fileID);
+%     disp('binary data!') % frewind(fileID);
     tline{Line}=tmpchar(1:2:end); % Convert
     data.rawfile.type='binary';
     data.rawfile.isbinary=1;
 else
-    disp('ascii data!')
+%     disp('ascii data!')
     tline{Line}=tmpchar; % Convert
     data.rawfile.type='ascii';
     data.rawfile.isbinary=0;
@@ -64,6 +65,13 @@ end
 % 
 data.rawfile.lines=tline;
 
+if contains(tline{10},'time') % Exist time vector?
+    data.containstime=1; % Yes
+else
+    data.containstime=0; % No
+end
+  
+
 
 % data.dataArray=dataArray;
 % toc
@@ -78,16 +86,29 @@ data.flags=linestr{2};
 linestr = strtrim(strsplit(tline{5},':')); % No. Variables:
 data.nvars=str2double(linestr{2});
 
+% 
 % Gets variables name
 vars=cell(1,data.nvars); % reserve space
 Line=data.rawfile.valuesline-data.nvars-1;
-for nv=1:data.nvars % Why 9?
-    vars{nv}=strsplit(strtrim(tline{Line}),char(9));
-    U = matlab.lang.makeValidName(char(vars{nv}(2)),'ReplacementStyle','delete');
-    data.signals(nv).label=char(U);
-    data.signals(nv).type=char(vars{nv}(3));
-    data.signals(nv).ID=str2double(char(vars{nv}(1)));
-    Line=Line+1;
+if data.containstime % Time data
+    Line=Line+1; % skip time variable
+    for nv=2:data.nvars %
+        vars{nv}=strsplit(strtrim(tline{Line}),char(9));
+        U = matlab.lang.makeValidName(char(vars{nv}(2)),'ReplacementStyle','delete');
+        data.signals(nv-1).label=char(U);
+        data.signals(nv-1).type=char(vars{nv}(3));
+%         data.signals(nv-1).ID=str2double(char(vars{nv}(1)));
+        Line=Line+1;
+    end
+else % No time data
+    for nv=1:data.nvars % OK
+        vars{nv}=strsplit(strtrim(tline{Line}),char(9));
+        U = matlab.lang.makeValidName(char(vars{nv}(2)),'ReplacementStyle','delete');
+        data.signals(nv).label=char(U);
+        data.signals(nv).type=char(vars{nv}(3));
+%         data.signals(nv).ID=str2double(char(vars{nv}(1)));
+        Line=Line+1;
+    end
 end
 
 linestr = strtrim(strsplit(tline{6},':')); % No. Points:
@@ -102,7 +123,7 @@ data.command=linestr{2};
 %% Get data binary
 
 if data.rawfile.isbinary
-    disp('RAW file is in binary format!')
+%     disp('RAW file is in binary format!')
     fseek(fileID,data.rawfile.valuesposition+1, 'bof'); % data position
     vardata = fread(fileID, 1, 'double'); % .op
     
@@ -115,10 +136,10 @@ if data.rawfile.isbinary
     end
     
     fclose(fileID);
-    fprintf('finished extracting LTspice ASCII results after %d seconds.\n',toc(tstart));
+    fprintf('Finished extracting LTspice ASCII results after %d seconds.\n',toc(tstart));
     return
 else
-    disp('RAW file is in ascii format!')
+%     disp('RAW file is in ascii format!')
 end
 
 
@@ -127,25 +148,19 @@ dlines=length(dataArray{1,1}); % Number of data lines
 ind=1:data.nvars:dlines; % Index for time data points
 data.id=dataArray{1,1}(ind(:));
 
-if contains(tline{10},'time')
-    data.time=dataArray{1,3}(ind(:));
-    data.Ts=data.time(2)-data.time(1);
+if data.containstime % contains(tline{10},'time')
+    data.time=dataArray{1,3}(ind(:)); % Extracts time 
+    data.Ts=mean(diff(data.time));
     for nv=2:data.nvars
-        indv=nv:data.nvars:dlines;
-        data.signals(nv-1).values=dataArray{1,2}(indv(:));
-%         data.signals(nv-1).mean=mean(data.signals(nv-1).values);
-%         data.signals(nv-1).rms=rms(data.signals(nv-1).values);        
-    end
+        indv=nv:data.nvars:dlines;  % Create variables indices in data array
+        data.signals(nv-1).values=dataArray{1,2}(indv(:));     
+    end    
     
-    
-else % Its an .op simulation
-%     data.time=[];
-%     data.Ts=[];
+else % No time vector
     dataArray{1,2}(1)=dataArray{1,3}(1); % Correct first data row
     for nv=1:data.nvars
-        indv=nv:data.nvars:dlines;
-        data.signals(nv).op=dataArray{1,2}(indv(:)); % Reads data points in .op simulation
-        data.signals(nv).values=dataArray{1,2}(indv(:)); % Reads data points in .op simulation        
+        indv=nv:data.nvars:dlines; % Create variables indices in data array
+        data.signals(nv).op=dataArray{1,2}(indv(:)); % Reads data points in .op simulation     
     end       
 end
 
@@ -155,6 +170,6 @@ end
 
 data.dataArray=dataArray;
 
-fprintf('finished extracting LTspice ASCII results after %d seconds.\n',toc(tstart));
+fprintf('Finished extracting LTspice ASCII results after %d seconds.\n',toc(tstart));
 
 
