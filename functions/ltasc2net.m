e777f4ddb43c57c54bef8b0c31bc57ea738414b9
Adriano Ruseler <adrianoruseler@gamil.com>
2019-04-10 13:53:53 -0300

    Update ltasc2net.m

--- functions/ltasc2net.m
+++ functions/ltasc2net.m
@@ -2,7 +2,7 @@
 % Generates netlist file .net from asc file
 function [circuit] = ltasc2net(circuit)
 
-circuit.LTspice.ascfile = [circuit.LTspice.simsdir '\' circuit.LTspice.name '.asc']; % Sim base file
+circuit.LTspice.ascfile = [circuit.LTspice.simsdir circuit.LTspice.name '.asc']; % Sim base file
 
 if ~exist(circuit.LTspice.ascfile,'file') % Caso arquivo não exista
     disp(['File not found: ' circuit.LTspice.ascfile])
@@ -16,7 +16,7 @@ disp(cmdout)
 
 if ~status % Se simulação foi bem sucedida
     circuit.LTspice.net.name = circuit.LTspice.name;
-    circuit.LTspice.net.file = [circuit.LTspice.simsdir '\' circuit.LTspice.name '.net']; % Sim base file
+    circuit.LTspice.net.file = [circuit.LTspice.simsdir circuit.LTspice.name '.net']; % Sim base file
 else
     circuit.LTspice.net.file='';
     return;
