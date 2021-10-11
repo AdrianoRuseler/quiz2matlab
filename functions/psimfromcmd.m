5319d1102ae6d76f1f17e263d785a31bf0a32d82
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-11 17:37:17 -0300

    Update psimfromcmd.m

--- functions/psimfromcmd.m
+++ functions/psimfromcmd.m
@@ -2,7 +2,7 @@
 % ***
 % *** The MIT License (MIT)
 % ***
-% *** Copyright (c) 2019 AdrianoRuseler
+% *** Copyright (c) 2021 AdrianoRuseler
 % ***
 % *** Permission is hereby granted, free of charge, to any person obtaining a copy
 % *** of this software and associated documentation files (the "Software"), to deal
@@ -28,10 +28,10 @@
 function  circuit = psimfromcmd(circuit)
 
 % Copyright ® 2006-2020 Powersim Inc.  All Rights Reserved.
-% 
+%
 % Usage: PsimCmd.exe -i "[input file]" -o "[output file]" -v "VarName1=VarValue"  -v "VarName2=VarValue"  -g -K1 -L1 -t "TotalTime" -s "TimeStep" -pt "PrintTime" -ps "PrintStep" -Net "Netlist file name" -m "file name for errors"
-% 
-% 
+%
+%
 % Except input file, all other parameters are optional.
 % All file names should be enclosed by " or ' characters.
 % Command-line parameters:
@@ -86,17 +86,20 @@ simfilebase = [circuit.PSIMCMD.simsdir circuit.PSIMCMD.name '.psimsch']; % Sim b
 circuit.PSIMCMD.inifile = [circuit.PSIMCMD.simsdir circuit.PSIMCMD.name '.ini']; % Arquivo ini simview
 
 if(circuit.PSIMCMD.tmpdir)  % Use system temp dir?
-   circuit.PSIMCMD.simsdir = tempdir;    
+    circuit.PSIMCMD.simsdir = tempdir;
 end
 
+randname = strrep(char(java.util.UUID.randomUUID),'-','');
+tmpname=[circuit.PSIMCMD.name randname];
+circuit.PSIMCMD.randname=randname;
+circuit.PSIMCMD.tmpname=tmpname;
 
-tmpname=[circuit.PSIMCMD.name strrep(char(java.util.UUID.randomUUID),'-','')];
 if(circuit.PSIMCMD.net.run) % run net file
     circuit.PSIMCMD.infile = [circuit.PSIMCMD.simsdir tmpname '.cct'];
     copyfile(circuit.PSIMCMD.net.file,circuit.PSIMCMD.infile)
 else
-    if(circuit.PSIMCMD.tmpfile) % Create tmp file for simulation?        
-        circuit.PSIMCMD.infile = [circuit.PSIMCMD.simsdir tmpname '.psimsch'];        
+    if(circuit.PSIMCMD.tmpfile) % Create tmp file for simulation?
+        circuit.PSIMCMD.infile = [circuit.PSIMCMD.simsdir tmpname '.psimsch'];
         copyfile(simfilebase,circuit.PSIMCMD.infile) % Copia arquivo
     else
         tmpname = circuit.PSIMCMD.name;
@@ -107,16 +110,28 @@ end
 circuit.PSIMCMD.outfile = [circuit.PSIMCMD.simsdir tmpname '.txt'];
 circuit.PSIMCMD.msgfile = [circuit.PSIMCMD.simsdir tmpname 'msg.txt'];
 
-% Cria string de comando
-infile = ['"' circuit.PSIMCMD.infile '"'];
-outfile = ['"' circuit.PSIMCMD.outfile '"'];
-msgfile = ['"' circuit.PSIMCMD.msgfile '"'];
-totaltime = ['"' num2str(circuit.PSIMCMD.totaltime,'%10.8e') '"'];  %   -t :  Followed by total time of the simulation.
-steptime = ['"' num2str(circuit.PSIMCMD.steptime,'%10.8e') '"']; %   -s :  Followed by time step of the simulation.
-printtime = ['"' num2str(circuit.PSIMCMD.printtime,'%10.8e') '"']; %   -pt : Followed by print time of the simulation.
-printstep = ['"' num2str(circuit.PSIMCMD.printstep,'%10.8e') '"']; %   -ps : Followed by print step of the simulation.
 
-PsimCmdsrt= ['-i ' infile ' -o ' outfile ' -m ' msgfile ' -t ' totaltime ' -s ' steptime ' -pt ' printtime ' -ps ' printstep ' ' circuit.PSIMCMD.extracmd];
+if circuit.PSIMCMD.script.run
+    circuit.PSIMCMD.script.file = [circuit.PSIMCMD.simsdir tmpname '.script'];
+    circuit.PSIMCMD.script.tmpname = tmpname;
+    circuit.PSIMCMD.script.name=['PS' upper(randname(1:6)) '.script'];
+    % Creates PSIM script
+    circuit = generatepsimscript(circuit);
+    PsimCmdsrt = circuit.PSIMCMD.script.file;
+else
+
+    % Cria string de comando
+    infile = ['"' circuit.PSIMCMD.infile '"'];
+    outfile = ['"' circuit.PSIMCMD.outfile '"'];
+    msgfile = ['"' circuit.PSIMCMD.msgfile '"'];
+    totaltime = ['"' num2str(circuit.PSIMCMD.totaltime,'%10.8e') '"'];  %   -t :  Followed by total time of the simulation.
+    steptime = ['"' num2str(circuit.PSIMCMD.steptime,'%10.8e') '"']; %   -s :  Followed by time step of the simulation.
+    printtime = ['"' num2str(circuit.PSIMCMD.printtime,'%10.8e') '"']; %   -pt : Followed by print time of the simulation.
+    printstep = ['"' num2str(circuit.PSIMCMD.printstep,'%10.8e') '"']; %   -ps : Followed by print step of the simulation.
+
+    PsimCmdsrt= ['-i ' infile ' -o ' outfile ' -m ' msgfile ' -t ' totaltime ' -s ' steptime ' -pt ' printtime ' -ps ' printstep ' ' circuit.PSIMCMD.extracmd];
+
+end
 
 tic
 disp(PsimCmdsrt)
@@ -186,14 +201,14 @@ for i=2:length(header)
     circuit.PSIMCMD.data.signals(i-1).meanround=round(mean(M(:,i)),0);
     circuit.PSIMCMD.data.signals(i-1).rms=rms(M(:,i));
     circuit.PSIMCMD.data.signals(i-1).max=max(M(:,i));
-    circuit.PSIMCMD.data.signals(i-1).min=min(M(:,i)); % 
-    circuit.PSIMCMD.data.signals(i-1).delta=abs( max(M(:,i)) - min(M(:,i)) ); % 
-%     circuit.PSIMCMD.data.signals(i-1).std=std(M(:,i)); % Standard deviation
-%     circuit.PSIMCMD.data.signals(i-1).median=median(M(:,i)); % Standard deviation
+    circuit.PSIMCMD.data.signals(i-1).min=min(M(:,i)); %
+    circuit.PSIMCMD.data.signals(i-1).delta=abs( max(M(:,i)) - min(M(:,i)) ); %
+    %     circuit.PSIMCMD.data.signals(i-1).std=std(M(:,i)); % Standard deviation
+    %     circuit.PSIMCMD.data.signals(i-1).median=median(M(:,i)); % Standard deviation
     circuit.PSIMCMD.data.signals(i-1).dimensions=1;
     circuit.PSIMCMD.data.signals(i-1).title=U;
     circuit.PSIMCMD.data.signals(i-1).plotStyle=[0,0];
-    
+
     disp(circuit.PSIMCMD.data.signals(i-1))
 end
 
@@ -208,6 +223,9 @@ if(circuit.PSIMCMD.tmpfiledel)
     delete(circuit.PSIMCMD.infile) % Deleta arquivo de simulação
     delete(circuit.PSIMCMD.outfile) % Deleta arquivo de dados
     delete(circuit.PSIMCMD.msgfile)
+    if circuit.PSIMCMD.script.run
+        delete(circuit.PSIMCMD.script.file)
+    end
 end
 disp('Done!!!!')
 disp(circuit.PSIMCMD)
