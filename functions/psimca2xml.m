821301c6c51a53021373fc75e1a65dd6db713b6f
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-11 18:10:21 -0300

    Update psimca2xml.m

--- functions/psimca2xml.m
+++ functions/psimca2xml.m
@@ -1,4 +1,28 @@
-
+% =========================================================================
+% ***
+% *** The MIT License (MIT)
+% ***
+% *** Copyright (c) 2021 AdrianoRuseler
+% ***
+% *** Permission is hereby granted, free of charge, to any person obtaining a copy
+% *** of this software and associated documentation files (the "Software"), to deal
+% *** in the Software without restriction, including without limitation the rights
+% *** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% *** copies of the Software, and to permit persons to whom the Software is
+% *** furnished to do so, subject to the following conditions:
+% ***
+% *** The above copyright notice and this permission notice shall be included in all
+% *** copies or substantial portions of the Software.
+% ***
+% *** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% *** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% *** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% *** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% *** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% *** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% *** SOFTWARE.
+% ***
+% =========================================================================
 
 function psimca2xml(circuit,quiz)
 
@@ -9,7 +33,7 @@ circuit.PSIMCMD.simsdir=circuit.dir; % PSIM file dir
 % Simulation control settings
 circuit.PSIMCMD.tmpfile=1; % Create tmp file?
 circuit.PSIMCMD.tmpdir=1; % Use system temp dir?
-circuit.PSIMCMD.tmpfiledel=0; % Delete tmp files?
+circuit.PSIMCMD.tmpfiledel=1; % Delete tmp files?
 
 % circuit.PSIMCMD.totaltime=10E-005; % Total simulation time, in sec.
 circuit.PSIMCMD.steptime=5E-007; % Simulation time step, in sec.
