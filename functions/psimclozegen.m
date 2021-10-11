df58ee1e1fbe31254d8b4c4183b54b5f85bfb143
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-11 17:37:13 -0300

    Update psimclozegen.m

--- functions/psimclozegen.m
+++ functions/psimclozegen.m
@@ -1,4 +1,28 @@
-
+% =========================================================================
+% ***
+% *** The MIT License (MIT)
+% ***
+% *** Copyright (c) 2021 AdrianoRuseler
+% ***
+% *** Permission is hereby granted, free of charge, to any person obtaining a copy
+% *** of this software and associated documentation files (the "Software"), to deal
+% *** in the Software without restriction, including without limitation the rights
+% *** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% *** copies of the Software, and to permit persons to whom the Software is
+% *** furnished to do so, subject to the following conditions:
+% ***
+% *** The above copyright notice and this permission notice shall be included in all
+% *** copies or substantial portions of the Software.
+% ***
+% *** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% *** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% *** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% *** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% *** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% *** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% *** SOFTWARE.
+% ***
+% =========================================================================
 
 function quizstruct = psimclozegen(circuits,quizopts)
 
@@ -27,7 +51,7 @@ for q=1:quizopts.nquiz
                 tmpstr=[tmpstr '(' strrep(tmpcircuits{q}.model(ml).parstr,' ','') ')'];
             end
             quizstruct.question.name{q}=[tmpcircuits{q}.quiz.name 'q' num2str(q,'%03i') '(' strrep(tmpcircuits{q}.parstr,' ','') ')'  tmpstr ]; % Generates quiz name
-            
+
         else
             quizstruct.question.name{q}=[tmpcircuits{q}.quiz.name 'q' num2str(q,'%03i') '(' strrep(tmpcircuits{q}.parstr,' ','') ')']; % Generates quiz name
         end
@@ -36,14 +60,19 @@ for q=1:quizopts.nquiz
     else
         quizstruct.question.name{q}=[tmpcircuits{q}.quiz.name 'q' num2str(q,'%03i')]; % Generates quiz name
     end
-        
+
     quizstruct.question.text{q}=tmpcircuits{q}.quiz.text;
 
     if tmpcircuits{q}.quiz.modelfile % Add model file with link to it
         quizstruct.question.filebase64code{q}=tmpcircuits{q}.model(1).filebase64code;
         quizstruct.question.filename{q} = tmpcircuits{q}.quiz.nomearquivo;
     end
-    
+
+    if tmpcircuits{q}.quiz.scriptfile % Add PSIM script file with link to it
+        quizstruct.question.filebase64code{q}=tmpcircuits{q}.PSIMCMD.script.base64code;
+        quizstruct.question.filename{q} = tmpcircuits{q}.PSIMCMD.script.name;
+    end
+
     disp(quizstruct.question.name{q}) % Display question name
     quizstruct.question.generalfeedback{q}=quizopts.generalfeedback;
     quizstruct.question.penalty{q}=quizopts.penalty;
