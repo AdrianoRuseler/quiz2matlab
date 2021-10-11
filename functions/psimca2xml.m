3a0e85158990feb34a2bc8b4ee69c2c9c6282f4f
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-10-11 17:37:06 -0300

    Update psimca2xml.m

--- functions/psimca2xml.m
+++ functions/psimca2xml.m
@@ -9,16 +9,29 @@ circuit.PSIMCMD.simsdir=circuit.dir; % PSIM file dir
 % Simulation control settings
 circuit.PSIMCMD.tmpfile=1; % Create tmp file?
 circuit.PSIMCMD.tmpdir=1; % Use system temp dir?
-circuit.PSIMCMD.tmpfiledel=1; % Delete tmp files?
+circuit.PSIMCMD.tmpfiledel=0; % Delete tmp files?
 
 % circuit.PSIMCMD.totaltime=10E-005; % Total simulation time, in sec.
-circuit.PSIMCMD.steptime=5E-007; % Simulation time step, in sec.  
-% circuit.PSIMCMD.printtime=0; %Time from which simulation results are saved to the output file (default = 0). No output is saved before this time. 
-circuit.PSIMCMD.printstep=1; %Print step (default = 1). If the print step is set to 1, every data point will be saved to the output file. 
-% If it is 10, only one out of 10 data points will be saved. This helps to reduce the size of the output file. 
+circuit.PSIMCMD.steptime=5E-007; % Simulation time step, in sec.
+% circuit.PSIMCMD.printtime=0; %Time from which simulation results are saved to the output file (default = 0). No output is saved before this time.
+circuit.PSIMCMD.printstep=1; %Print step (default = 1). If the print step is set to 1, every data point will be saved to the output file.
+% If it is 10, only one out of 10 data points will be saved. This helps to reduce the size of the output file.
 
 circuit.PSIMCMD.net.run = 0; % Flag to use netlist file in simulation
 % circuit = getpsimnet(circuit); % Reads or generates net file from psim
+if ~isfield(circuit.PSIMCMD,'script')
+    circuit.PSIMCMD.script.run=0;  % Flag to use script file in simulation
+    quiz.scriptfile=0; % Add link to script file
+end
+
+if ~isfield(quiz,'modelfile') % Create file with model
+    quiz.modelfile=0;
+end
+
+if ~isfield(quiz,'scriptfile') % Create file with model
+    quiz.scriptfile=0;
+end
+
 
 % sortnquestions=300; % Number of simulations
 [~,y]=size(circuit.Xi);
@@ -35,14 +48,14 @@ for c=1:y
     tmpcircuits{c}.parvalue=circuit.X(:,c); % Variables values
     tmpcircuits{c}.parstr = param2str(tmpcircuits{c});
     tmpcircuits{c}.PSIMCMD.totaltime=circuit.cycles/tmpcircuits{c}.parvalue(circuit.fundfreqind); % Total simulation time, in sec.
-    tmpcircuits{c}.PSIMCMD.printtime=(circuit.printcycle-1)/tmpcircuits{c}.parvalue(circuit.fundfreqind);   
+    tmpcircuits{c}.PSIMCMD.printtime=(circuit.printcycle-1)/tmpcircuits{c}.parvalue(circuit.fundfreqind);
 end
 
 % Eval functions
-if isfield(circuit,'funcstr')     
+if isfield(circuit,'funcstr')
     for c=1:y
         parvalues = tmpcircuits{c}.parvalue; % Its used in function input eval
-        for f=1:length(circuit.funcstr) % Functions loop            
+        for f=1:length(circuit.funcstr) % Functions loop
             tmpcircuits{c}.funcvalue(f)=eval(circuit.funcstr{f}); % Eval functions
         end
     end
@@ -89,7 +102,7 @@ quiz.name = [circuit.name];
 % Auto add item letter: a), b)... 97 - 122; 65 - 90
 if isfield(quiz,'autoitem') && quiz.autoitem
     for q=1:length(quiz.question)
-        quiz.question{q}.str=[ char(96+q) ') ' quiz.question{q}.str]; 
+        quiz.question{q}.str=[ char(96+q) ') ' quiz.question{q}.str];
         disp(quiz.question{q}.str)
     end
 end
@@ -97,9 +110,9 @@ end
 for n=1:length(circuits)
     circuits{n}.quiz=quiz;
     figlegendastr=['Figura 1: Considere ' circuits{n}.parstr ';']; % Legenda da figura
-    circuits{n}.quiz.fightml = psimfigstr(imgout,'left',figlegendastr); % html code for fig     
-    circuits{n} = psimXmultichoice(circuits{n}); % Generate multichoice        
-    circuits{n} = quiztextgen(circuits{n}); % Generates quiz text field    
+    circuits{n}.quiz.fightml = psimfigstr(imgout,'left',figlegendastr); % html code for fig
+    circuits{n} = psimXmultichoice(circuits{n}); % Generate multichoice
+    circuits{n} = quiztextgen(circuits{n}); % Generates quiz text field
 end
 
 %% Generate quizstruct moodle question
