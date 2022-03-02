2b8ae4729bf890877d1363a4991958e39ef8540c
Adriano Ruseler <ruseler@inep.ufsc.br>
2022-03-02 14:49:17 -0300

    Update getsimdir.m

--- functions/getsimdir.m
+++ functions/getsimdir.m
@@ -1,22 +1,13 @@
 
-function  filepath = getsimdir(filename,software)
+function  filepath = getsimdir(filename)
+
+% circuit.dir = [pwd '\'];  % Sets simulation dir
 
 % software ='PSIM' 'LTspice' 'Ngspice'
 str=which(filename); % find filename dir
 
 if isempty(str)
-    %     [str,maxsize,endian] = computer
-    [~, pcname] = system('hostname');
-    if contains(pcname,'WIN10-GAMER','IgnoreCase',true) % Home PC
-        filepath=['F:\Dropbox\GitHub\quiz2matlab\sims\' software '\'];
-    elseif contains(pcname,'DESKTOP-RUSELER','IgnoreCase',true)  % UTFPR
-        filepath=['A:\Dropbox\GitHub\quiz2matlab\sims\' software '\'];
-    elseif contains(pcname,'RUSELER-NOTE','IgnoreCase',true)  % Note
-        filepath=['C:\Users\adria\Dropbox\GitHub\quiz2matlab\sims\' software '\'];
-    else
-        filepath = pwd; %
-    end
-    % circuit.dir='A:\Dropbox\GitHub\quiz2matlab\sims\PSIM\';
+    filepath = [pwd '\'];  % Sets simulation dir
 else
     filepath = fileparts(str);
     filepath = [filepath '\'];
