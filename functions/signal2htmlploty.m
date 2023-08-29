2e62270680c61c3c5d4b7df5e30bca6b6a8ce19f
Adriano Ruseler <adrianoruseler@gmail.com>
2023-08-29 15:47:43 -0300

    Create signal2htmlploty.m

---
+++ functions/signal2htmlploty.m
@@ -0,0 +1,43 @@
+function ploty=signal2htmlploty(plotytdata)
+
+% plotytdata=circuits{1}.PSIMCMD.data;
+
+t=1;
+htmlploty{t}=['<div id="plotly-' plotytdata.blockName '"></div>']; t=t+1;
+htmlploty{t}='<script>'; t=t+1;
+
+vars={plotytdata.signals.label}; % Get variables
+nvars=length(vars); % number of variables
+
+tracex = ['x: ['  regexprep(num2str(plotytdata.time'),'\s+',', ') '],'];
+
+for v=1:nvars
+    htmlploty{t}=['var trace' num2str(v,'%02i') ' = {']; t=t+1;
+    htmlploty{t}=tracex; t=t+1;
+    tracey = ['y: ['  regexprep(num2str(plotytdata.signals(v).values'),'\s+',', ') '],'];
+    htmlploty{t}=tracey; t=t+1;
+    htmlploty{t}='mode: ''lines'','; t=t+1;
+    htmlploty{t}=['name: ''' vars{v} '''' ]; t=t+1;
+    htmlploty{t}='};'; t=t+1;
+end
+
+htmlploty{t}='var data = [trace01';
+for v=2:nvars
+    htmlploty{t}=[htmlploty{t} ', trace' num2str(v,'%02i')];
+end
+htmlploty{t}=[htmlploty{t} '];'];
+t=t+1;
+
+htmlploty{t}=['var layout = { title:''' plotytdata.blockName ''' };']; t=t+1;
+htmlploty{t}=['Plotly.newPlot(''plotly-' plotytdata.blockName ''' , data)']; t=t+1;
+htmlploty{t}='</script>'; % t=t+1;
+
+ploty='';
+for t=1:length(htmlploty)
+    ploty=[ploty htmlploty{t}];
+end
+
+
+% for i=1:length(htmlploty)
+%     disp(htmlploty{i})
+% end
