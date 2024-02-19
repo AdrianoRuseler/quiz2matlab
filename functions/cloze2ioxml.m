8cd1d99b10d35fb146c3b720b9682227583eeb17
Adriano Ruseler <adrianoruseler@gmail.com>
2024-02-19 09:39:01 -0300

    Update cloze2ioxml.m

--- functions/cloze2ioxml.m
+++ functions/cloze2ioxml.m
@@ -16,8 +16,9 @@
 %
 % =========================================================================
 
-function cloze2ioxml(clozes) % Generate xml file
+function XMLfile=cloze2ioxml(clozes) % Generate xml file
 
+XMLfile='';
 
 if isfield(clozes,'xmlpath')
     if ~exist(clozes.xmlpath,'dir')
@@ -35,6 +36,24 @@ else
     return
 end
 
+% Init all nom existing or empty fields
+for q=1:nq
+    % if ~isfield(clozes.q(q),'comment') || isempty(clozes.q(q).comment)
+    %     clozes.q(q).comment='';
+    % end
+    if ~isfield(clozes.q(q),'generalfeedback') || isempty(clozes.q(q).generalfeedback)
+        clozes.q(q).generalfeedback='';
+    end
+    if ~isfield(clozes.q(q),'penalty') || isempty(clozes.q(q).penalty)
+        clozes.q(q).penalty='0.3333333';
+    end
+    if ~isfield(clozes.q(q),'hidden') || isempty(clozes.q(q).hidden)
+        clozes.q(q).hidden='0';
+    end
+    % if ~isfield(clozes.q(q),'idnumber') || isempty(clozes.q(q).idnumber)
+    %     clozes.q(q).idnumber='';
+    % end
+end
 
 % Create the document node and root element, toc:
 import matlab.io.xml.dom.*
@@ -48,7 +67,7 @@ dt = char(datetime('now','Format','yyyMMddHHmmss'));
 
 for q=1:nq
     % Add question comment
-    if isfield(clozes.q(q),'comment')
+    if isfield(clozes.q(q),'comment') & ~isempty(clozes.q(q).comment)
         appendChild(docRootNode,createComment(docNode,clozes.q(q).comment));
     end
 
@@ -75,37 +94,36 @@ for q=1:nq
     generalfeedback = createElement(docNode,'generalfeedback');
     setAttribute(generalfeedback,'format','html');
     generalfeedbacktext = createElement(docNode,'text');
-    if isfield(clozes.q(q),'generalfeedback')
+    % if isfield(clozes.q(q),'generalfeedback')
         appendChild(generalfeedbacktext,createTextNode(docNode,clozes.q(q).generalfeedback)); % Coloca feedback da pergunta
-    end
+    % end
     appendChild(generalfeedback,generalfeedbacktext);
     appendChild(question,generalfeedback);
 
     % Question penalty
     penalty = createElement(docNode,'penalty');
-    if isfield(clozes.q(q),'penalty')
+    % if isfield(clozes.q(q),'penalty')
         appendChild(penalty,createTextNode(docNode,clozes.q(q).penalty));
-    else
-        appendChild(penalty,createTextNode(docNode,'0.3333333'));
-    end
+    % else
+    %     appendChild(penalty,createTextNode(docNode,'0.3333333'));
+    % end
     appendChild(question,penalty);
 
     % Question hidden
     hidden = createElement(docNode,'hidden');
-    if isfield(clozes.q(q),'hidden')
+    % if isfield(clozes.q(q),'hidden')
         appendChild(hidden,createTextNode(docNode,clozes.q(q).hidden));
-    else
-        appendChild(hidden,createTextNode(docNode,'0'));
-    end
+    % else
+    %     appendChild(hidden,createTextNode(docNode,'0'));
+    % end
     appendChild(question,hidden);
 
     % Question idnumber
     idnumber = createElement(docNode,'idnumber');
-    if isfield(clozes.q(q),'idnumber')
+    if isfield(clozes.q(q),'idnumber') & ~isempty(clozes.q(q).idnumber)
         appendChild(idnumber,createTextNode(docNode,clozes.q(q).idnumber));
     end
     appendChild(question,idnumber);
-
 
     appendChild(docRootNode,question);
 end
@@ -116,5 +134,8 @@ XMLfile=[clozes.xmlpath '\' clozes.fname 'D' dt 'NQ' num2str(nq,'%03i') '.xml'];
 writeToFile(writer,docNode,XMLfile);
 
 % type(XMLfile);
+xml2html(XMLfile)
 
 winopen(clozes.xmlpath)
+
+
