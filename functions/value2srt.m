a81561180d4c5e23d8c17d07e4df8f762d1e10e0
Adriano Ruseler <adrianoruseler@gmail.com>
2024-06-25 14:32:15 -0300

    Update value2srt.m

--- functions/value2srt.m
+++ functions/value2srt.m
@@ -4,9 +4,14 @@ exponent = 3*floor(log10(abs(number))/3); % find exponent
 mantissa = number/(10^exponent); % find mantissa
 
 if isinf(exponent) % If is inf!
-    valuesrt= '0';
+    valuesrt= 'inf';
 else
     UnitPrefix = {'y','z', 'a', 'f', 'p', 'n', 'u', 'm', '', 'k', 'MEG', 'G', 'T', 'P', 'E', 'Z','Y'};
-    valuesrt=[num2str(mantissa) UnitPrefix{exponent/3 +9}];
+    indx=exponent/3 +9;
+    if indx<18
+        valuesrt=[num2str(mantissa) UnitPrefix{indx}];
+    else
+        valuesrt=[num2str(mantissa) 'e' num2str(exponent)];
+    end
 end
 % disp(valuesrt)
