6be145b1996880fccde39d53b7f4499174dd11d5
Adriano Ruseler <adrianoruseler@gmail.com>
2024-08-07 14:29:56 -0300

    Create functionName.m
    Template

---
+++ functions/functionName.m
@@ -0,0 +1,33 @@
+function [output1, output2, ..., outputN] = functionName(input1, input2, ..., inputM)
+% FUNCTIONNAME Brief description of the function's purpose.
+%
+%   Syntax:
+%   [output1, output2, ..., outputN] = FUNCTIONNAME(input1, input2, ..., inputM)
+%
+%   Description:
+%   Detailed description of the function, including its main purpose and
+%   any important details about its operation.
+%
+%   Inputs:
+%   input1 - Description of input1, including data type and dimensions.
+%   input2 - Description of input2, including data type and dimensions.
+%   ...
+%   inputM - Description of inputM, including data type and dimensions.
+%
+%   Outputs:
+%   output1 - Description of output1, including data type and dimensions.
+%   output2 - Description of output2, including data type and dimensions.
+%   ...
+%   outputN - Description of outputN, including data type and dimensions.
+%
+%   Example:
+%   Provide a simple example of how to use the function. This could be
+%   a single line of code or a short script that demonstrates typical use.
+%
+%   See also: List related functions or files, if applicable.
+%
+%   Author: Your Name
+%   Date: Date of creation or last modification
+%   Version: Version number
+
+% Your code here
