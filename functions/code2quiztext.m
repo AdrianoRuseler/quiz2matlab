69077849f27cb59639c2634727a8378b98072d41
Adriano Ruseler <ruseler@inep.ufsc.br>
2025-06-02 11:35:26 -0300

    Update code2quiztext.m

--- functions/code2quiztext.m
+++ functions/code2quiztext.m
@@ -2,20 +2,63 @@ function codestr= code2quiztext(cfile,style)
 
 switch nargin
     case 2
+        disp('Case 2')
         codestr=fileread(cfile);
         cstyle=lower(style);
-        codestr=formatcode(code,style);
+        codestr=formatcode(codestr,style);
     case 1
+        disp('Case 1')
         codestr=fileread(cfile);
     otherwise
         codestr='';
         return
 end
 
-% HTML escape: sign < as &lt;, the greater-than sign > as &gt;
-% codestr=replace(codestr,'<','&lt;');
-% codestr=replace(codestr,'>','&gt;');
+% escapeHTML Escapes HTML special characters in a string.
+codestr = escapeHTML(codestr);
 
-codestr = escapeHTML(codestr)
+% escapeHTML
 
-escapeHTML
+% codesample_languages: [
+%     { text: 'HTML/XML', value: 'markup' },
+%     { text: 'JavaScript', value: 'javascript' },
+%     { text: 'CSS', value: 'css' },
+%     { text: 'PHP', value: 'php' },
+%     { text: 'Ruby', value: 'ruby' },
+%     { text: 'Python', value: 'python' },
+%     { text: 'Java', value: 'java' },
+%     { text: 'C', value: 'c' },
+%     { text: 'C#', value: 'csharp' },
+%     { text: 'C++', value: 'cpp' }
+%   ],
+
+[~, ~, lang_ext] = fileparts(cfile);
+
+switch lower(lang_ext)
+    case {'.html', '.htm', '.xml', '.xhtml'}
+        category = 'markup';
+    case {'.js'}
+        category = 'javascript';
+    case {'.css'}
+        category = 'css';
+    case {'.php', '.php4', '.php5', '.phtml'}
+        category = 'php';
+    case {'.rb'}
+        category = 'ruby';
+    case {'.py'}
+        category = 'python';
+    case {'.java'}
+        category = 'java';
+    case {'.c', '.h'}
+        category = 'c';
+    case {'.cs'}
+        category = 'csharp';
+    case {'.cpp', '.cc', '.cxx', '.hpp', '.hxx'}
+        category = 'cpp';
+    otherwise
+        category = 'markup'; % Or you could return an error or a default category
+end
+
+% disp(category)
+
+codestr = ['<pre class="language-' category '"><code>' codestr '</code></pre>'];
