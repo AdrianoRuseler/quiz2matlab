2fc8c1b55ad74532b22383b0a1859625887af97b
Adriano Ruseler <adrianoruseler@gmail.com>
2025-07-01 11:05:31 -0300

    Update stack2ioxml.m

--- functions/stack2ioxml.m
+++ functions/stack2ioxml.m
@@ -29,96 +29,8 @@ else
     stacks.xmlpath=pwd;
 end
 
-% Number os questions
-if isfield(stacks,'q')
-    nq=length(stacks.q);
-else
-    disp('No questions in cloze struct!')
-    return
-end
+stacks=stackverify(stacks);
 
-% Verify for usetidy
-if isfield(stacks,'usetidy')
-    % https://github.com/htacg/tidy-html5
-    [status,tidyver] = system('tidy --version');
-    if status
-        disp(tidyver)
-        disp('https://github.com/htacg/tidy-html5')
-        stacks.usetidy=0;
-    else
-        stacks.usetidy=1;
-    end
-else
-    stacks.usetidy=0;
-end
-
-% Init all nom existing or empty fields
-for q=1:nq
-    % if ~isfield(stacks.q(q),'comment') || isempty(stacks.q(q).comment)
-    %     stacks.q(q).comment='';
-    % end
-    if ~isfield(stacks.q(q),'generalfeedback') || isempty(stacks.q(q).generalfeedback)
-        stacks.q(q).generalfeedback='';
-    end
-    if ~isfield(stacks.q(q),'penalty') || isempty(stacks.q(q).penalty)
-        stacks.q(q).penalty='0.3333333';
-    end
-    if ~isfield(stacks.q(q),'hidden') || isempty(stacks.q(q).hidden)
-        stacks.q(q).hidden='0';
-    end
-    if ~isfield(stacks.q(q),'defaultgrade') || isempty(stacks.q(q).defaultgrade)
-        stacks.q(q).defaultgrade='';
-    end
-    if ~isfield(stacks.q(q),'idnumber') || isempty(stacks.q(q).idnumber)
-        stacks.q(q).idnumber='';
-    end
-    if stacks.usetidy
-        stacks.q(q).text = formathtml(stacks.q(q).text);
-    end
-
-    if ~isfield(stacks.q(q),'questionsimplify') || isempty(stacks.q(q).questionsimplify)
-        stacks.q(q).questionsimplify='0';
-    end
-    if ~isfield(stacks.q(q),'assumepositive') || isempty(stacks.q(q).assumepositive)
-        stacks.q(q).assumepositive='0';
-    end
-    if ~isfield(stacks.q(q),'assumereal') || isempty(stacks.q(q).assumereal)
-        stacks.q(q).assumereal='0';
-    end
-
-    if ~isfield(stacks.q(q),'decimals') || isempty(stacks.q(q).decimals)
-        stacks.q(q).decimals='.';
-    end
-    if ~isfield(stacks.q(q),'scientificnotation') || isempty(stacks.q(q).scientificnotation)
-        stacks.q(q).scientificnotation='*10';
-    end
-    if ~isfield(stacks.q(q),'multiplicationsign') || isempty(stacks.q(q).multiplicationsign)
-        stacks.q(q).multiplicationsign='dot';
-    end
-    if ~isfield(stacks.q(q),'sqrtsign') || isempty(stacks.q(q).sqrtsign)
-        stacks.q(q).sqrtsign='1';
-    end
-    if ~isfield(stacks.q(q),'complexno') || isempty(stacks.q(q).complexno)
-        stacks.q(q).complexno='i';
-    end
-    if ~isfield(stacks.q(q),'inversetrig') || isempty(stacks.q(q).inversetrig)
-        stacks.q(q).inversetrig='cos-1';
-    end
-    if ~isfield(stacks.q(q),'logicsymbol') || isempty(stacks.q(q).logicsymbol)
-        stacks.q(q).logicsymbol='lang';
-    end
-    if ~isfield(stacks.q(q),'matrixparens') || isempty(stacks.q(q).matrixparens)
-        stacks.q(q).matrixparens='[';
-    end
-    if ~isfield(stacks.q(q),'variantsselectionseed') || isempty(stacks.q(q).variantsselectionseed)
-        stacks.q(q).variantsselectionseed='';
-    end
-
-    if ~isfield(stacks.q(q),'input') || isempty(stacks.q(q).input)
-        stacks.q(q).input='';
-    end
-
-end
 
 % Create the document node and root element, toc:
 import matlab.io.xml.dom.*
@@ -130,6 +42,7 @@ writer.Configuration.FormatPrettyPrint = true;
 
 dt = char(datetime('now','Format','yyyMMddHHmmss'));
 
+nq=length(stacks.q);
 for q=1:nq
     % Add question comment
     if isfield(stacks.q(q),'comment') & ~isempty(stacks.q(q).comment)
@@ -386,35 +299,174 @@ for q=1:nq
     appendChild(prtname,createTextNode(docNode,stacks.q(q).prt.name));
     appendChild(prt,prtname);
 
-      % <value>1.0000000</value>
+    % <value>1.0000000</value>
     prtvalue = createElement(docNode,'value');
     appendChild(prtvalue,createTextNode(docNode,stacks.q(q).prt.value));
     appendChild(prt,prtvalue);
 
-      % <autosimplify>0</autosimplify>
+    % <autosimplify>0</autosimplify>
     prtautosimplify = createElement(docNode,'autosimplify');
     appendChild(prtautosimplify,createTextNode(docNode,stacks.q(q).prt.autosimplify));
     appendChild(prt,prtautosimplify);
-      % <feedbackstyle>1</feedbackstyle>
+    % <feedbackstyle>1</feedbackstyle>
     prtfeedbackstyle = createElement(docNode,'feedbackstyle');
     appendChild(prtfeedbackstyle,createTextNode(docNode,stacks.q(q).prt.feedbackstyle));
     appendChild(prt,prtfeedbackstyle);
-      % <feedbackvariables>
-      %   <text></text>
-      % </feedbackvariables>
+    % <feedbackvariables>
+    %   <text></text>
+    % </feedbackvariables>
     prtfeedbackvariables = createElement(docNode,'feedbackvariables');
     prtfeedbackvariablestext = createElement(docNode,'text');
     appendChild(prtfeedbackvariablestext,createTextNode(docNode,stacks.q(q).prt.feedbackvariables));
     appendChild(prtfeedbackvariables,prtfeedbackvariablestext);
     appendChild(prt,prtfeedbackvariables);
 
+    nqnodes=length(stacks.q(q).prt.node);
+    for no=1:nqnodes
+        nodeElement = createElement(docNode,'node');
+        nodeElementname = createElement(docNode,'name');
+        appendChild(nodeElementname,createTextNode(docNode,stacks.q(q).prt.node(no).name));
+        appendChild(nodeElement,nodeElementname);
+
+        nodeElementdescription = createElement(docNode,'description');
+        appendChild(nodeElementdescription,createTextNode(docNode,stacks.q(q).prt.node(no).description));
+        appendChild(nodeElement,nodeElementdescription);
+
+        nodeElementanswertest = createElement(docNode,'answertest');
+        appendChild(nodeElementanswertest,createTextNode(docNode,stacks.q(q).prt.node(no).answertest));
+        appendChild(nodeElement,nodeElementanswertest);
+
+        nodeElementsans = createElement(docNode,'sans');
+        appendChild(nodeElementsans,createTextNode(docNode,stacks.q(q).prt.node(no).sans));
+        appendChild(nodeElement,nodeElementsans);
+
+        nodeElementtans = createElement(docNode,'tans');
+        appendChild(nodeElementtans,createTextNode(docNode,stacks.q(q).prt.node(no).tans));
+        appendChild(nodeElement,nodeElementtans);
+
+        nodeElementtestoptions = createElement(docNode,'testoptions');
+        appendChild(nodeElementtestoptions,createTextNode(docNode,stacks.q(q).prt.node(no).testoptions));
+        appendChild(nodeElement,nodeElementtestoptions);
+
+        nodeElementquiet = createElement(docNode,'quiet');
+        appendChild(nodeElementquiet,createTextNode(docNode,stacks.q(q).prt.node(no).quiet));
+        appendChild(nodeElement,nodeElementquiet);
+
+        nodeElementtruescoremode = createElement(docNode,'truescoremode');
+        appendChild(nodeElementtruescoremode,createTextNode(docNode,stacks.q(q).prt.node(no).truescoremode));
+        appendChild(nodeElement,nodeElementtruescoremode);
+
+        nodeElementtruescore = createElement(docNode,'truescore');
+        appendChild(nodeElementtruescore,createTextNode(docNode,stacks.q(q).prt.node(no).truescore));
+        appendChild(nodeElement,nodeElementtruescore);
+
+        nodeElementtruepenalty = createElement(docNode,'truepenalty');
+        appendChild(nodeElementtruepenalty,createTextNode(docNode,stacks.q(q).prt.node(no).truepenalty));
+        appendChild(nodeElement,nodeElementtruepenalty);
+
+        nodeElementtruenextnode = createElement(docNode,'truenextnode');
+        appendChild(nodeElementtruenextnode,createTextNode(docNode,stacks.q(q).prt.node(no).truenextnode));
+        appendChild(nodeElement,nodeElementtruenextnode);
+
+        nodeElementtruefeedback = createElement(docNode,'truefeedback');
+        setAttribute(nodeElementtruefeedback,'format','html');
+        nodeElementtruefeedbacktext = createElement(docNode,'text');
+        appendChild(nodeElementtruefeedbacktext,createTextNode(docNode,stacks.q(q).prt.node(no).truefeedback));
+        appendChild(nodeElementtruefeedback,nodeElementtruefeedbacktext);
+        appendChild(nodeElement,nodeElementtruefeedback);
 
 
+        nodeElementfalsescoremode = createElement(docNode,'falsescoremode');
+        appendChild(nodeElementfalsescoremode,createTextNode(docNode,stacks.q(q).prt.node(no).falsescoremode));
+        appendChild(nodeElement,nodeElementfalsescoremode);
+
+        nodeElementfalsescore = createElement(docNode,'falsescore');
+        appendChild(nodeElementfalsescore,createTextNode(docNode,stacks.q(q).prt.node(no).falsescore));
+        appendChild(nodeElement,nodeElementfalsescore);
+
+        nodeElementfalsepenalty = createElement(docNode,'falsepenalty');
+        appendChild(nodeElementfalsepenalty,createTextNode(docNode,stacks.q(q).prt.node(no).falsepenalty));
+        appendChild(nodeElement,nodeElementfalsepenalty);
+
+        nodeElementfalsenextnode = createElement(docNode,'falsenextnode');
+        appendChild(nodeElementfalsenextnode,createTextNode(docNode,stacks.q(q).prt.node(no).falsenextnode));
+        appendChild(nodeElement,nodeElementfalsenextnode);
+
+        nodeElementfalseanswernote = createElement(docNode,'falseanswernote');
+        appendChild(nodeElementfalseanswernote,createTextNode(docNode,stacks.q(q).prt.node(no).falseanswernote));
+        appendChild(nodeElement,nodeElementfalseanswernote);
+
+        nodeElementfalsefeedback = createElement(docNode,'falsefeedback');
+        setAttribute(nodeElementfalsefeedback,'format','html');
+        nodeElementfalsefeedbacktext = createElement(docNode,'text');
+        appendChild(nodeElementfalsefeedbacktext,createTextNode(docNode,stacks.q(q).prt.node(no).falsefeedback));
+        appendChild(nodeElementfalsefeedback,nodeElementfalsefeedbacktext);
+        appendChild(nodeElement,nodeElementfalsefeedback);
+
+        appendChild(prt,nodeElement);
+    end
     appendChild(question,prt);
 
+    % stacks.q(1).deployedseed(1).value='1795421824';
+    nseeds=length(stacks.q(q).deployedseed);
+    for ns=1:nseeds
+        deployedseedElement = createElement(docNode,'deployedseed');
+        appendChild(deployedseedElement,createTextNode(docNode,stacks.q(q).deployedseed(ns).value));
+        appendChild(question,deployedseedElement);
+    end
+
+    nts=length(stacks.q(q).qtest);
+    for nt=1:nts
+        qtestElement = createElement(docNode,'qtest');
+
+        qtestElementtestcase = createElement(docNode,'testcase');
+        appendChild(qtestElementtestcase,createTextNode(docNode,stacks.q(q).qtest(nt).testcase));
+        appendChild(qtestElement,qtestElementtestcase);
+
+        qtestElementdescription = createElement(docNode,'description');
+        appendChild(qtestElementdescription,createTextNode(docNode,stacks.q(q).qtest(nt).description));
+        appendChild(qtestElement,qtestElementdescription);
+
+        % stacks.q(1).qtest(qt).testinput.name='ans';
+        % stacks.q(1).qtest(qt).testinput.value='ta';
+        qtestElementtestinput = createElement(docNode,'testinput');
+        qtestElementtestinputtext = createElement(docNode,'text');
+        appendChild(qtestElementtestinputtext,createTextNode(docNode,stacks.q(q).qtest(nt).testinput.name));
+        qtestElementtestinputvalue = createElement(docNode,'value');
+        appendChild(qtestElementtestinputvalue,createTextNode(docNode,stacks.q(q).qtest(nt).testinput.value));
+
+        appendChild(qtestElementtestinput,qtestElementtestinputtext);
+        appendChild(qtestElementtestinput,qtestElementtestinputvalue);
+        appendChild(qtestElement,qtestElementtestinput);
+
+        
+        % stacks.q(1).qtest(qt).expected.name='prt1';
+        % stacks.q(1).qtest(qt).expected.expectedscore='1.0000000';
+        % stacks.q(1).qtest(qt).expected.expectedpenalty='0.0000000';
+        % stacks.q(1).qtest(qt).expected.expectedanswernote='1-0-T';
+
+        qtestElementexpected = createElement(docNode,'expected');
+        qtestElementexpectedname = createElement(docNode,'name');
+        qtestElementexpectedexpectedscore = createElement(docNode,'expectedscore');
+        qtestElementexpectedexpectedpenalty = createElement(docNode,'expectedpenalty');
+        qtestElementexpectedexpectedanswernote = createElement(docNode,'expectedanswernote');
+
+        appendChild(qtestElementexpectedname,createTextNode(docNode,stacks.q(q).qtest(nt).expected.name));
+        appendChild(qtestElementexpectedexpectedscore,createTextNode(docNode,stacks.q(q).qtest(nt).expected.expectedscore));
+        appendChild(qtestElementexpectedexpectedpenalty,createTextNode(docNode,stacks.q(q).qtest(nt).expected.expectedpenalty));
+        appendChild(qtestElementexpectedexpectedanswernote,createTextNode(docNode,stacks.q(q).qtest(nt).expected.expectedanswernote));
+
+        appendChild(qtestElementexpected,qtestElementexpectedname);
+        appendChild(qtestElementexpected,qtestElementexpectedexpectedscore);
+        appendChild(qtestElementexpected,qtestElementexpectedexpectedpenalty);
+        appendChild(qtestElementexpected,qtestElementexpectedexpectedanswernote);
+
+        appendChild(qtestElement,qtestElementexpected);
 
 
-    % stacks.q(1).input.type='algebraic';
+        appendChild(question,qtestElement);
+    end
+
 
 
     appendChild(docRootNode,question);
